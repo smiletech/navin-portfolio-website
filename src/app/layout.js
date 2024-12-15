0d8e5e2699b9733e163f9b291bfdce4bb0eab3cb
Navin Kumar <navinsmileraj@gmail.com>
2024-12-15 21:00:46 +0530

    update packages and comment email route

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'navin portfolio',
   description: 'navin-portfolio with personal details',
+  icons: {
+    icon: '/favicon.ico', 
+  },
 }
 
 export default function RootLayout({ children }) {
